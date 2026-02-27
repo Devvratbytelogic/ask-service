488cc9b28da97fe139ae9e55a68c543d87bfab26
Devvratbytelogic <142411170+Devvratbytelogic@users.noreply.github.com>
2026-02-27 18:26:35 +0530

    update API base URL

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const GOOGLE_API_KEY = "AIzaSyBYTVr8YYdf7qm_TrZdTBKV07S-apoYEbs"
 
-// export const API_BASE_URL = "https://ask.webdesignnoida.in/api"
-export const API_BASE_URL = "http://192.168.1.30:3200/api"
+export const API_BASE_URL = "https://ask.webdesignnoida.in/api"
+// export const API_BASE_URL = "http://192.168.1.30:3200/api"
