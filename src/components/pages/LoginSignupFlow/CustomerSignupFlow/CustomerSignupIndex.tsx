3b89ea7cc62d4f8213e0530aea46e151cbffef61
Devvratbytelogic <142411170+Devvratbytelogic@users.noreply.github.com>
2026-03-02 17:41:37 +0530

    google login done 	modified:   src/components/common/Header/Header.tsx 	modified:   src/components/pages/LoginSignupFlow/CustomerSignInFlow/CustomerSignInIndex.tsx 	modified:   src/components/pages/LoginSignupFlow/CustomerSignupFlow/CustomerSignupIndex.tsx

--- src/components/pages/LoginSignupFlow/CustomerSignupFlow/CustomerSignupIndex.tsx
+++ src/components/pages/LoginSignupFlow/CustomerSignupFlow/CustomerSignupIndex.tsx
@@ -1,7 +1,11 @@
 import ImageComponent from "@/components/library/ImageComponent"
 import { RootState } from "@/redux/appStore"
-import { openModal } from "@/redux/slices/allModalSlice"
-import { Button } from "@heroui/react"
+import { closeModal, openModal } from "@/redux/slices/allModalSlice"
+import { setAuthCookies, type AuthResponseData } from "@/utils/authCookies"
+import { loginWithGoogle } from "@/firebase/GoogleLogin"
+import { addToast, Button } from "@heroui/react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 import { BiArrowBack, BiPhone } from "react-icons/bi"
 import { CgMail } from "react-icons/cg"
 import { useDispatch, useSelector } from "react-redux"
@@ -9,8 +13,37 @@ import { useDispatch, useSelector } from "react-redux"
 const CustomerSignupIndex = () => {
 
     const { data } = useSelector((state: RootState) => state.allCommonModal);
-
     const dispatch = useDispatch();
+    const router = useRouter();
+    const [isGoogleLoading, setIsGoogleLoading] = useState(false);
+
+    const userType = data?.userData?.userType as string | undefined;
+    const roleType = userType === "service" ? "Vendor" : "User";
+
+    const handleGoogleLogin = async () => {
+        setIsGoogleLoading(true);
+        try {
+            const res = await loginWithGoogle(roleType);
+            const responseData = res?.data;
+            if (responseData && typeof responseData === "object") {
+                const authData = responseData as AuthResponseData;
+                if (authData.token ?? authData.access_token) {
+                    setAuthCookies(authData);
+                    router.refresh();
+                    addToast({ title: "Account created successfully", color: "success", timeout: 2000 });
+                    dispatch(closeModal());
+                    return;
+                }
+            }
+            addToast({ title: "Sign up completed", color: "success", timeout: 2000 });
+            dispatch(closeModal());
+        } catch (err: unknown) {
+            const message = (err as Error & { responseData?: { message?: string } })?.responseData?.message ?? (err as Error)?.message ?? "Google sign up failed";
+            addToast({ title: message, color: "danger", timeout: 3000 });
+        } finally {
+            setIsGoogleLoading(false);
+        }
+    };
 
     const signupForUserWith = (userSignupType: string) => {
         dispatch(openModal({
@@ -45,7 +78,12 @@ const CustomerSignupIndex = () => {
             <div className="w-full space-y-4.5">
                 {/* Continue with Google */}
                 <section className="w-11/12">
-                    <Button className="btn_bg_white btn_radius btn_padding w-full font-medium">
+                    <Button
+                        className="btn_bg_white btn_radius btn_padding w-full font-medium"
+                        onPress={handleGoogleLogin}
+                        isLoading={isGoogleLoading}
+                        isDisabled={isGoogleLoading}
+                    >
                         <span className="size-4.5"><ImageComponent url="/images/signup/google_icon.png" img_title="google login icon" /></span>Continue with Google
                     </Button>
                 </section>
