6714e77d50a32656054632e9e93fa19211b7f51b
Devvratbytelogic <support@bytelogicindia.com>
2026-07-29 11:30:23 +0530

    update service provider page

--- src/app/service-provider/page.tsx
+++ src/app/service-provider/page.tsx
@@ -17,10 +17,10 @@ export default async function ServiceProviderPage() {
     const activeVendorsCount = globalSettings?.data?.total_active_vendors ?? 0
     const activeRequestsCount = globalSettings?.data?.total_service_requests ?? 0
     const averageRating = globalSettings?.data?.average_customer_satisfaction_score ?? 0
-    const averageResponseTime = globalSettings?.data?.average_time_to_receive_a_quote ?? 0
+    // const averageResponseTime = globalSettings?.data?.average_time_to_receive_a_quote ?? 0
     return (
         <>
-            <ServiceProviderHero activeVendorsCount={activeVendorsCount} activeRequestsCount={activeRequestsCount} averageRating={averageRating} averageResponseTime={averageResponseTime} />
+            <ServiceProviderHero activeVendorsCount={activeVendorsCount} activeRequestsCount={activeRequestsCount} averageRating={averageRating}  />
             <ServiceProviderHowItWorks />
             <ServiceProviderBenefits />
             <ServiceProviderTestimonials />
