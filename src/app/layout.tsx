e8b097d6110ffb72a4e533f8672c18c228978f6b
Devvratbytelogic <support@bytelogicindia.com>
2026-08-05 16:52:36 +0530

    fix: suppress hydration warning in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({
   const marketplaceName = globalSettings?.data?.marketplace_name;
 
   return (
-    <html lang="fr" translate="no" suppressHydrationWarning>
+    <html lang="fr"  suppressHydrationWarning>
       <body
         className={`${bricolageGrotesque.variable} font-sans antialiased`}
       >
